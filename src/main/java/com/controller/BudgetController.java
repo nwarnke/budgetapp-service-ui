198f35d34f7157916da1b75ec95e5adf4e262db9
Nick <nwarnke@unomaha.edu>
2016-07-22 21:11:10 -0500

    Add TODO and begin implementation of newBudget method

--- src/main/java/com/controller/BudgetController.java
+++ src/main/java/com/controller/BudgetController.java
@@ -4,15 +4,19 @@ import com.dao.IBudgetDao;
 import com.dao.ICategoryDao;
 import com.dto.Budget;
 import com.dto.Category;
+import com.service.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = {"http://localhost:9000", "https://budget-management-app.herokuapp.com"})
 @RequestMapping("/budget")
 public class BudgetController {
 
@@ -25,8 +29,8 @@ public class BudgetController {
         this.categoryDao = categoryDao;
     }
 
-    @RequestMapping(value="/getbudget", method = RequestMethod.GET)
-    public Budget getBudget(HttpServletRequest httpServletRequest){
+    @RequestMapping(value = "/getbudget", method = RequestMethod.GET)     //TODO incomplete method
+    public Budget getBudget(HttpServletRequest httpServletRequest) {
         Budget budget = new Budget();
         final String userid = (String) httpServletRequest.getSession().getAttribute("userid");
         final List<Category> categoriesForUser = categoryDao.findCategoriesForUser(userid);
@@ -35,6 +39,19 @@ public class BudgetController {
         return budget;
     }
 
+    @RequestMapping(value = "/newbudget", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity newBudget(HttpServletRequest httpServletRequest, @RequestParam("budgetName") String budgetName,
+                                    @RequestParam("amount") String amount,
+                                    @RequestParam("startDate") String startDate,
+                                    @RequestParam("endDate") String endDate) {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
 
+
+
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
 
 }
