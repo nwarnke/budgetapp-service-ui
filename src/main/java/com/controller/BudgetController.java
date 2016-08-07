fc5887828f18e578d9e60d5e838d14834619b370
Jenni <jhizer@unomaha.edu>
2016-08-07 14:29:27 -0500

    Committing new methods in Budget Controller, add, update, delete

--- src/main/java/com/controller/BudgetController.java
+++ src/main/java/com/controller/BudgetController.java
@@ -6,7 +6,6 @@ import com.dao.ISubcategoryDao;
 import com.dto.Budget;
 import com.dto.Category;
 import com.dto.Subcategory;
-import com.dto.User;
 import com.service.Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,14 +43,14 @@ public class BudgetController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         final Budget budget = budgetDao.lookupBudget(budgetId);
-        List<Category> categories = categoryDao.findCategoriesForBudgetId(budgetId);
+        List<Category> categories = categoryDao.getCategories(budgetId);
         if(categories == null) {
             categories = new ArrayList<>();
         }
         budget.setCategories(categories);
         for(Category category: budget.getCategories()){
             String categoryId = String.valueOf(category.getCategoryId());
-            List<Subcategory> subcategories = subcategoryDao.findSubCategoriesForCategoryId(categoryId);
+            List<Subcategory> subcategories = subcategoryDao.getSubCategories(categoryId);
             if(subcategories == null) {
                 subcategories = new ArrayList<>();
             }
@@ -60,24 +59,120 @@ public class BudgetController {
         return new ResponseEntity<>(budget, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/newbudget", method = RequestMethod.POST)
+    @RequestMapping(value = "/addcategory", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity newBudget(HttpServletRequest httpServletRequest, @RequestParam("budgetName") String budgetName,
-                                    @RequestParam("amount") String amount,
+    public ResponseEntity addCategory(HttpServletRequest httpServletRequest, @RequestParam("categoryName") String categoryName,
+                                    @RequestParam("limit") String limit,
+                                    @RequestParam("expenses") String expenses,
                                     @RequestParam("startDate") String startDate,
-                                    @RequestParam("endDate") String endDate) throws ParseException {
+                                    @RequestParam("endDate") String endDate,
+                                    @RequestParam("budgetId") String budgetId) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        Date startDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+        Date endDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+
+        final int created = categoryDao.addCategory(categoryName, limit, expenses, startDateUtil, endDateUtil, budgetId);
+
+        if(created == 1) {
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/addsubcategory", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity addSubcategory(HttpServletRequest httpServletRequest, @RequestParam("subcategoryName") String subcategoryName,
+                                         @RequestParam("limit") String limit,
+                                         @RequestParam("expenses") String expenses,
+                                         @RequestParam("startDate") String startDate,
+                                         @RequestParam("endDate") String endDate,
+                                         @RequestParam("categoryId") String categoryId) throws ParseException {
         if(!Service.isAuthenticatedUser(httpServletRequest)){
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         Date startDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
         Date endDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
 
-        final int updated = budgetDao.addBudget(budgetName, amount, startDateUtil, endDateUtil, ((User) httpServletRequest.getSession().getAttribute("userInfo")).getUserId());
+        final int created = subcategoryDao.addSubcategory(subcategoryName, limit, expenses, startDateUtil, endDateUtil, categoryId);
+
+        if(created == 1) {
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/updatecategory", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity updateCategory(HttpServletRequest httpServletRequest, @RequestParam("categoryId") String categoryId,
+                                         @RequestParam("categoryName") String categoryName,
+                                         @RequestParam("limit") String limit,
+                                         @RequestParam("expenses") String expenses,
+                                         @RequestParam("startDate") String startDate,
+                                         @RequestParam("endDate") String endDate) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Date startDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+        Date endDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+
+        final int updated = categoryDao.updateCategory(categoryId, categoryName, limit, expenses, startDateUtil, endDateUtil);
 
         if(updated == 1) {
             return new ResponseEntity(HttpStatus.ACCEPTED);
-        }else{
-            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/updatesubcategory", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity updateSubcategory(HttpServletRequest httpServletRequest, @RequestParam("subcategoryId") String subcategoryId,
+                                         @RequestParam("subcategoryName") String subcategoryName,
+                                         @RequestParam("limit") String limit,
+                                         @RequestParam("expenses") String expenses,
+                                         @RequestParam("startDate") String startDate,
+                                         @RequestParam("endDate") String endDate) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Date startDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+        Date endDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+
+        final int updated = subcategoryDao.updateSubcategory(subcategoryId, subcategoryName, limit, expenses, startDateUtil, endDateUtil);
+
+        if(updated == 1) {
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/deletecategory", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity deleteCategory(HttpServletRequest httpServletRequest, @RequestParam("categoryId") String categoryId) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        final int deleted = categoryDao.deleteCategory(categoryId);
+        if(deleted == 1) {
+            final int deletedSubCategories = subcategoryDao.deleteSubcategoryByParentId(categoryId);
+            if(deletedSubCategories == 1){
+                return new ResponseEntity(HttpStatus.ACCEPTED);
+            }
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/deletesubcategory", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity deleteSubcategory(HttpServletRequest httpServletRequest, @RequestParam("subcategoryId") String subcategoryId) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        final int deleted = subcategoryDao.deleteSubcategory(subcategoryId);
+        if(deleted == 1) {
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/test/java/com/controller/BudgetControllerTest.java
+++ src/test/java/com/controller/BudgetControllerTest.java
@@ -9,7 +9,6 @@ import com.dao.impl.SubcategoryDao;
 import com.dto.Budget;
 import com.dto.Category;
 import com.dto.Subcategory;
-import com.dto.User;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
@@ -55,11 +54,11 @@ public class BudgetControllerTest {
       {
         oneOf(budgetDao).lookupBudget(with(budgetId));
         will(returnValue(budget));
-        oneOf(categoryDao).findCategoriesForBudgetId(with(budgetId));
+        oneOf(categoryDao).getCategories(with(budgetId));
         will(returnValue(categories));
-        oneOf(subcategoryDao).findSubCategoriesForCategoryId(with(categoryId));
+        oneOf(subcategoryDao).getSubCategories(with(categoryId));
         will(returnValue(subcategories));
-        oneOf(subcategoryDao).findSubCategoriesForCategoryId(with(secondCategoryId));
+        oneOf(subcategoryDao).getSubCategories(with(secondCategoryId));
         will(returnValue(subcategories));
       }
     });
@@ -70,38 +69,140 @@ public class BudgetControllerTest {
   }
 
   @Test
-  public void testNewBudget() throws Exception {
+  public void testAddCategory() throws Exception {
     MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
     mockHttpServletRequest.getSession().setAttribute("authenticated", true);
-    final User user = createUser();
-    mockHttpServletRequest.getSession().setAttribute("userInfo", user);
 
-    final String budgetName = "Test budget";
-    final String amount = "1600";
+    final String categoryName = "Test category";
+    final String limit = "1600";
+    final String expenses = "200";
     final String startDate = "2016-07-06T05:00:00.000Z";
     final String endDate = "2016-07-27T05:00:00.000Z";
+    final String budgetId = "1";
 
     context.checking(new Expectations(){
       {
-        oneOf(budgetDao).addBudget(with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)), with(any(String.class)));
+        oneOf(categoryDao).addCategory(with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)), with(any(String.class)));
         will(returnValue(1));
       }
     });
 
-    final ResponseEntity responseEntity = budgetController.newBudget(mockHttpServletRequest, budgetName, amount, startDate, endDate);
+    final ResponseEntity responseEntity = budgetController.addCategory(mockHttpServletRequest, categoryName, limit, expenses, startDate, endDate, budgetId);
     assertEquals(202, responseEntity.getStatusCodeValue());
     context.assertIsSatisfied();
   }
 
-  private User createUser() {
-    final User user = new User();
-    user.setPassword("password");
-    user.setUserId("test");
-    user.setFirstName("john");
-    user.setLastName("smith");
-    user.setUserName("testing");
-    return user;
+    @Test
+    public void testAddSubCategory() throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+        final String subcategoryName = "Test subcategory";
+        final String limit = "400";
+        final String expenses = "50";
+        final String startDate = "2016-07-06T05:00:00.000Z";
+        final String endDate = "2016-07-27T05:00:00.000Z";
+        final String categoryId = "12";
+
+        context.checking(new Expectations(){
+            {
+                oneOf(subcategoryDao).addSubcategory(with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)), with(any(String.class)));
+                will(returnValue(1));
+            }
+        });
+
+        final ResponseEntity responseEntity = budgetController.addSubcategory(mockHttpServletRequest, subcategoryName, limit, expenses, startDate, endDate, categoryId);
+        assertEquals(202, responseEntity.getStatusCodeValue());
+        context.assertIsSatisfied();
+    }
+
+  @Test
+  public void testUpdateCategory() throws Exception {
+    MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+    mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+    final String categoryId = "12";
+    final String categoryName = "Test category update";
+    final String amount = "500";
+    final String limit = "1000";
+    final String startDate = "2016-07-06T05:00:00.000Z";
+    final String endDate = "2016-07-27T05:00:00.000Z";
+
+    context.checking(new Expectations(){
+        {
+            oneOf(categoryDao).updateCategory(with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)));
+            will(returnValue(1));
+        }
+    });
+
+    final ResponseEntity responseEntity = budgetController.updateCategory(mockHttpServletRequest, categoryId, categoryName, amount, limit, startDate, endDate);
+    assertEquals(202, responseEntity.getStatusCodeValue());
+    context.assertIsSatisfied();
   }
+
+    @Test
+    public void testUpdateSubcategory() throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+        final String subcategoryId = "5";
+        final String subcategoryName = "Test subcategory update";
+        final String amount = "10";
+        final String limit = "100";
+        final String startDate = "2016-07-06T05:00:00.000Z";
+        final String endDate = "2016-07-27T05:00:00.000Z";
+
+        context.checking(new Expectations(){
+            {
+                oneOf(subcategoryDao).updateSubcategory(with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)));
+                will(returnValue(1));
+            }
+        });
+
+        final ResponseEntity responseEntity = budgetController.updateSubcategory(mockHttpServletRequest, subcategoryId, subcategoryName, amount, limit, startDate, endDate);
+        assertEquals(202, responseEntity.getStatusCodeValue());
+        context.assertIsSatisfied();
+    }
+
+  @Test
+  public void testDeleteCategory() throws Exception {
+    MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+    mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+    final String categoryId = "3";
+
+    context.checking(new Expectations(){
+      {
+        oneOf(categoryDao).deleteCategory(with(any(String.class)));
+        will(returnValue(1));
+        oneOf(subcategoryDao).deleteSubcategoryByParentId(with(any(String.class)));
+        will(returnValue(1));
+      }
+    });
+
+    final ResponseEntity responseEntity = budgetController.deleteCategory(mockHttpServletRequest, categoryId);
+    assertEquals(202, responseEntity.getStatusCodeValue());
+    context.assertIsSatisfied();
+  }
+
+    @Test
+    public void testDeleteSubcategory() throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+        final String subcategoryId = "3";
+
+        context.checking(new Expectations(){
+            {
+                oneOf(subcategoryDao).deleteSubcategory(with(any(String.class)));
+                will(returnValue(1));
+            }
+        });
+
+        final ResponseEntity responseEntity = budgetController.deleteSubcategory(mockHttpServletRequest, subcategoryId);
+        assertEquals(202, responseEntity.getStatusCodeValue());
+        context.assertIsSatisfied();
+    }
 
   private Budget createBudget() {
     final Budget budget = new Budget();
@@ -116,13 +217,12 @@ public class BudgetControllerTest {
   }
 
   private List<Category> createCategories(){
-    final List<Category> categories = new ArrayList<Category>();
+    final List<Category> categories = new ArrayList<>();
       final Category category = new Category();
       category.setCategoryId(1);
       category.setCategoryName("Car");
       category.setCategoryExpenses(100);
       category.setCategoryLimit(200);
-      //category.setSubcategories(createSubcategories());
       categories.add(category);
 
       final Category secondCategory = new Category();
@@ -130,14 +230,13 @@ public class BudgetControllerTest {
       secondCategory.setCategoryName("Food");
       secondCategory.setCategoryExpenses(160);
       secondCategory.setCategoryLimit(300);
-      //secondCategory.setSubcategories(null);
       categories.add(secondCategory);
       return categories;
   }
 
 
   private List<Subcategory> createSubcategories(){
-      final List<Subcategory> subcategories = new ArrayList<Subcategory>();
+      final List<Subcategory> subcategories = new ArrayList<>();
       final Subcategory subcategory = new Subcategory();
       subcategory.setSubcategoryId(1);
       subcategory.setSubcategoryName("Gas");
