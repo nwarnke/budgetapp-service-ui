b310ddabca30a351ae2401c22ae010b6bad1a906
Jenni <jhizer@unomaha.edu>
2016-08-07 14:30:01 -0500

    Committing new methods in Home Controller, add, update, delete

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,19 +1,23 @@
 package com.controller;
 
 import com.dao.IBudgetDao;
+import com.dao.ICategoryDao;
+import com.dao.ISubcategoryDao;
 import com.dto.Budget;
+import com.dto.Category;
 import com.dto.User;
 import com.service.Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -22,11 +26,15 @@ import java.util.List;
 public class HomeController {
 
   private IBudgetDao budgetDao;
+    private ICategoryDao categoryDao;
+    private ISubcategoryDao subcategoryDao;
 
   @Inject
-  public HomeController(IBudgetDao budgetDao) {
-    this.budgetDao = budgetDao;
-  }
+    public HomeController(IBudgetDao budgetDao, ICategoryDao categoryDao, ISubcategoryDao subcategoryDao) {
+        this.budgetDao = budgetDao;
+        this.categoryDao = categoryDao;
+        this.subcategoryDao = subcategoryDao;
+    }
 
   @RequestMapping(value="/budgets", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
@@ -37,5 +45,74 @@ public class HomeController {
     final User userInfo = (User) httpServletRequest.getSession().getAttribute("userInfo");
     return new ResponseEntity<>(budgetDao.getBudgets(userInfo.getUserId()), HttpStatus.OK);
   }
+
+  @RequestMapping(value = "/addbudget", method = RequestMethod.POST)
+  @ResponseBody
+  public ResponseEntity addBudget(HttpServletRequest httpServletRequest, @RequestParam("budgetName") String budgetName,
+                                  @RequestParam("amount") String amount,
+                                  @RequestParam("startDate") String startDate,
+                                  @RequestParam("endDate") String endDate) throws ParseException {
+    if(!Service.isAuthenticatedUser(httpServletRequest)){
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+    Date startDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+    Date endDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+
+    final int created = budgetDao.addBudget(budgetName, amount, startDateUtil, endDateUtil, ((User) httpServletRequest.getSession().getAttribute("userInfo")).getUserId());
+
+    if(created == 1) {
+      return new ResponseEntity(HttpStatus.ACCEPTED);
+    }else{
+      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+    @RequestMapping(value = "/updatebudget", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity updateBudget(HttpServletRequest httpServletRequest, @RequestParam("budgetId") String budgetId,
+                                @RequestParam("budgetName") String budgetName,
+                                @RequestParam("amount") String amount,
+                                @RequestParam("limit") String limit,
+                                @RequestParam("startDate") String startDate,
+                                @RequestParam("endDate") String endDate) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Date startDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+        Date endDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+        final int updated = budgetDao.updateBudget(budgetId, budgetName, amount, limit, startDateUtil, endDateUtil);
+        if(updated == 1) {
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/deletebudget", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity deleteBudget(HttpServletRequest httpServletRequest, @RequestParam("budgetId") String budgetId) throws ParseException {
+        if(!Service.isAuthenticatedUser(httpServletRequest)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        final int deleted = budgetDao.deleteBudget(budgetId);
+        if(deleted == 1) {
+            List<Category> categories = categoryDao.getCategories(budgetId);
+            if(categories != null) {
+                final int deletedCategories = categoryDao.deleteCategoryByParentId(budgetId);
+                if(deletedCategories == 1) {
+                    for(Category category: categories) {
+                        final int deletedSubCategories = subcategoryDao.deleteSubcategoryByParentId(String.valueOf(category.getCategoryId()));
+                        if (deletedSubCategories != 1) {
+                            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+                        }
+                    }
+                } else {
+                    return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            }
+        } else {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
 
 }
--- src/test/java/com/controller/HomeControllerTest.java
+++ src/test/java/com/controller/HomeControllerTest.java
@@ -1,8 +1,13 @@
 package com.controller;
 
 import com.dao.IBudgetDao;
+import com.dao.ICategoryDao;
+import com.dao.ISubcategoryDao;
 import com.dao.impl.BudgetDao;
+import com.dao.impl.CategoryDao;
+import com.dao.impl.SubcategoryDao;
 import com.dto.Budget;
+import com.dto.Category;
 import com.dto.User;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -12,6 +17,8 @@ import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -21,13 +28,17 @@ public class HomeControllerTest {
   private Mockery context;
   private HomeController homeController;
   private IBudgetDao budgetDao;
+  private ICategoryDao categoryDao;
+  private ISubcategoryDao subcategoryDao;
 
   @Before
   public void setUp() throws Exception {
     context = new Mockery();
     context.setImposteriser(ClassImposteriser.INSTANCE);
     budgetDao = context.mock(BudgetDao.class);
-    homeController = new HomeController(budgetDao);
+    categoryDao = context.mock(CategoryDao.class);
+    subcategoryDao = context.mock(SubcategoryDao.class);
+    homeController = new HomeController(budgetDao, categoryDao, subcategoryDao);
   }
 
   @Test
@@ -48,6 +59,82 @@ public class HomeControllerTest {
     context.assertIsSatisfied();
   }
 
+  @Test
+  public void testAddBudget() throws Exception {
+    MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+    mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+    final User user = createUser();
+    mockHttpServletRequest.getSession().setAttribute("userInfo", user);
+
+    final String budgetName = "Test budget";
+    final String amount = "1600";
+    final String startDate = "2016-07-06T05:00:00.000Z";
+    final String endDate = "2016-07-27T05:00:00.000Z";
+
+    context.checking(new Expectations(){
+      {
+        oneOf(budgetDao).addBudget(with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)), with(any(String.class)));
+        will(returnValue(1));
+      }
+    });
+
+    final ResponseEntity responseEntity = homeController.addBudget(mockHttpServletRequest, budgetName, amount, startDate, endDate);
+    assertEquals(202, responseEntity.getStatusCodeValue());
+    context.assertIsSatisfied();
+  }
+
+    @Test
+    public void testUpdateBudget() throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+        final String budgetId = "3";
+        final String budgetName = "Test budget";
+        final String amount = "200";
+        final String limit = "1400";
+        final String startDate = "2016-07-06T05:00:00.000Z";
+        final String endDate = "2016-07-27T05:00:00.000Z";
+
+        context.checking(new Expectations(){
+            {
+                oneOf(budgetDao).updateBudget(with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(String.class)), with(any(Date.class)), with(any(Date.class)));
+                will(returnValue(1));
+            }
+        });
+
+        final ResponseEntity responseEntity = homeController.updateBudget(mockHttpServletRequest, budgetId, budgetName, amount, limit, startDate, endDate);
+        assertEquals(202, responseEntity.getStatusCodeValue());
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testDeleteBudget() throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.getSession().setAttribute("authenticated", true);
+
+        final String budgetId = "3";
+        final List<Category> categories = createCategoryList();
+
+        context.checking(new Expectations(){
+            {
+                oneOf(budgetDao).deleteBudget(with(any(String.class)));
+                will(returnValue(1));
+                oneOf(categoryDao).getCategories(with(any(String.class)));
+                will(returnValue(categories));
+                oneOf(categoryDao).deleteCategoryByParentId(with(any(String.class)));
+                will(returnValue(1));
+                oneOf(subcategoryDao).deleteSubcategoryByParentId(with(any(String.class)));
+                will(returnValue(1));
+                oneOf(subcategoryDao).deleteSubcategoryByParentId(with(any(String.class)));
+                will(returnValue(1));
+            }
+        });
+
+        final ResponseEntity responseEntity = homeController.deleteBudget(mockHttpServletRequest, budgetId);
+        assertEquals(202, responseEntity.getStatusCodeValue());
+        context.assertIsSatisfied();
+    }
+
   private User createUser() {
     final User user = new User();
     user.setPassword("password");
@@ -58,5 +145,15 @@ public class HomeControllerTest {
     return user;
   }
 
+    private List<Category> createCategoryList() {
+        final List<Category> categories = new ArrayList<>();
+        Category category = new Category();
+        category.setCategoryId(1);
+        categories.add(category);
+        Category categorySecond = new Category();
+        category.setCategoryId(2);
+        categories.add(categorySecond);
+        return categories;
+    }
 
 }
